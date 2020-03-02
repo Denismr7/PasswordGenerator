26c99ca23d61953af16fd69c324c8d3864c477df
Denis Mory <denismr7@gmail.com>
2020-03-02 10:59:22 +0100

    now it saves the passwords in a txt file

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,5 +1,6 @@
 import random
 import string
+import json
 
 
 def generate_password(level):
@@ -10,9 +11,9 @@ def generate_password(level):
         for x in range(3):
             x = random.choice(letters)
             list.append(x)
-        password = str(numeric) + "".join(list)
+        generate_password.password = str(numeric) + "".join(list)
 
-        return password
+        return generate_password.password
     if level == "medium":
         list = []
         numeric = random.randint(0, 99999999)
@@ -20,9 +21,9 @@ def generate_password(level):
         for x in range(5):
             x = random.choice(letters)
             list.append(x)
-        password = str(numeric) + "".join(list)
+        generate_password.password = str(numeric) + "".join(list)
 
-        return password
+        return generate_password.password
     if level == "hard":
         list = []
         leftlist = []
@@ -34,21 +35,36 @@ def generate_password(level):
         for y in range(6):
             y = random.choice(letters)
             leftlist.append(y)
-        password = "".join(leftlist) + str(numeric) + "".join(list)
+        generate_password.password = "".join(leftlist) + str(numeric) + "".join(list)
 
-        return password
+        return generate_password.password
+
+
+def save():
+    save = input("Do you want to save the password? (Yes/No) ")
+    if save.lower() == "yes":
+        passname = str(input("Type the password's name: "))
+        data = list({passname + ": " + str(generate_password.password)})
+        print(data)
+        with open("passwords.txt", "a") as passwordlist:
+            passwordlist.write(json.dumps(data))
+            print(f"Password for {passname} saved!")
 
 
 print("Hi! Welcome to password generator, in order to bring you a safe password, first let us know which security "
       "level do you want your password to have")
 while True:
     level = input("Easy, Medium or Hard? ")
-    if level.lower() == "easy":
-        print("Your password is: " + generate_password("easy"))
-    if level.lower() == "medium":
-        print("Your password is: " + generate_password("medium"))
-    if level.lower() == "hard":
-        print("Your password is: " + generate_password("hard"))
+    if level.lower() in ("easy", "medium", "hard"):
+        if level.lower() == "easy":
+            print("Your password is: " + generate_password("easy"))
+            save()
+        if level.lower() == "medium":
+            print("Your password is: " + generate_password("medium"))
+            save()
+        if level.lower() == "hard":
+            print("Your password is: " + generate_password("hard"))
+            save()
     else:
         print("Invalid input")
 
